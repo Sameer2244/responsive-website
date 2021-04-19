7362a300c41d41b33355a91c5ab36642540f4fb5
sameer <sk5276873@gmail.com>
2021-04-19 20:48:06 +0530

    v1.1

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const btn=document.getElementsByClassName('dropdownbtn')[0];
 const links=document.getElementsByClassName('nav-links')[0];
 const container=document.getElementsByClassName('container')[0];
 const brand=document.getElementsByClassName('brand-name')[0];
+
 btn.addEventListener('click',()=>{
     links.classList.toggle('active');
     container.classList.toggle('smooth');
