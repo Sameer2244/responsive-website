42db73faabc444f8e1dc91ce50eb9485dd02bdd7
sameer <sk5276873@gmail.com>
2021-04-19 17:31:36 +0530

    init

---
+++ script.js
@@ -0,0 +1,9 @@
+const btn=document.getElementsByClassName('dropdownbtn')[0];
+const links=document.getElementsByClassName('nav-links')[0];
+const container=document.getElementsByClassName('container')[0];
+const brand=document.getElementsByClassName('brand-name')[0];
+btn.addEventListener('click',()=>{
+    links.classList.toggle('active');
+    container.classList.toggle('smooth');
+    brand.classList.toggle('anime');
+})
